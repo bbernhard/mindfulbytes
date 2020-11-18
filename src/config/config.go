860267885f7e8d159fa140b66008b87f105f46e1
Bernhard B <schluchti@gmail.com>
2020-11-18 23:15:44 +0100

    added config

---
+++ src/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+)
+
+type Notification struct {
+	Recipients []string `yaml:"recipients"`
+	Enabled bool `yaml:"enabled"`
+	Topics []string `yaml:"topics"`
+	Interval string `yaml:"interval"`
+	Settings map[string]string `yaml:"settings"`
+}
+
+type Config struct {
+	Notifications map[string]Notification `yaml:"notifications"`
+}
+
+func ParseConfig(pathToConfigFile string) (Config, error) {
+	var c Config
+	
+	data, err := ioutil.ReadFile(pathToConfigFile)
+	if err != nil {
+		return c, err
+	}
+	
+	err = yaml.Unmarshal([]byte(data), &c)
+	if err != nil {
+		return c, err
+	}
+
+	return c, nil
+}
